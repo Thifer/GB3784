d153dc28f240a71902075cc3b7de73bbc9760fc8
NoName <buybackbrain.genetic@gmail.com>
2023-01-17 02:44:44 +0300

    Init

---
+++ Les1/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите номер дня недели");
+int a = Int32.Parse(Console.ReadLine());
+switch (a)
+{
+    case 1: Console.WriteLine("Понедельник");
+        break;
+    case 2: Console.WriteLine("Вторник");
+        break;
+    case 3: Console.WriteLine("Среда");
+        break;
+    case 4: Console.WriteLine("Четверг");
+        break;
+    case 5: Console.WriteLine("Пятница");
+        break;
+    case 6: Console.WriteLine("Суббота");
+        break;
+    case 7: Console.WriteLine("Воскресенье");
+        break;
+    default: Console.WriteLine("что-то пошло не так");
+        break;
+}
---
+++ Les1/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+Console.WriteLine(s[s.Length - 1]);
+Console.WriteLine("Введите число");
+int b = Int32.Parse(Console.ReadLine());
+Console.WriteLine(b%10);
