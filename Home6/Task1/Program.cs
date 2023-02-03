dc765204cf0321ecc2db4380463fef723c5daf6e
NoName <buybackbrain.genetic@gmail.com>
2023-02-03 04:12:15 +0300

    home6 done

---
+++ Home6/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[4];
+int result = 0;
+
+Console.WriteLine("Введите 4 числа");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) result++;
+}
+
+Console.WriteLine(String.Join(", ",array));
+Console.WriteLine($"Чисел больше 0: {result}");
---
+++ Home6/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿int b1,k1,b2,k2 = 0;
+
+
+void collisionchech(int b1, int b2, int k1, int k2)
+{
+    if(k1==k2) Console.WriteLine("Прямые параллельны");
+    else
+    {
+        float x = (float)(b2 - b1) / (k1 - k2);
+        float y = (k1 * b2 - k2 * b1) / (float)(k1 - k2);
+        Console.WriteLine($"({x};{y})");        
+    }
+}
+
+int printtext(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine());
+}
+
+b1 = printtext("Введите b1");
+k1 = printtext("Введите k1");
+b2 = printtext("Введите b2");
+k2 = printtext("Введите k2");
+
+
+collisionchech(b1,b2,k1,k2);
+
+
+
