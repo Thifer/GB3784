e1a3ea97dbc268fa1d93f68c68cbb475b1423f8f
NoName <buybackbrain.genetic@gmail.com>
2023-02-03 10:10:15 +0300

    update

--- Home6/Task1/Program.cs
+++ Home6/Task1/Program.cs
@@ -1,17 +1,38 @@
-﻿int[] array = new int[4];
+﻿int Result()
+{
+    Int32[] int32s;
+    Int32 int32;
+    for (int i = 0; i < int32s.Length; i++)
+    {
+        if (int32s[i] > 0) int32++;
+    }
+
+    return int32;
+}
+
+void FillArr(int a)
+{
+    array = new int[a]
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+}
+
+void ShowArr(int[] arr)
+{
+    Console.WriteLine(String.Join(", ",arr));
+}
+
+
+int[] array;
 int result = 0;
 
-Console.WriteLine("Введите 4 числа");
 
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = int.Parse(Console.ReadLine());
-}
+Console.WriteLine("Введите кол-во чисел в массиве");
+FillArr(int.Parse(Console.ReadLine()));
+result = Result();
 
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i] > 0) result++;
-}
-
-Console.WriteLine(String.Join(", ",array));
+ShowArr(array);
 Console.WriteLine($"Чисел больше 0: {result}");
+
